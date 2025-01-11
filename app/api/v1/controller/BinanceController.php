752d0869579e4ce76d02e0f11764e530268abad5
dexcedu <shicms@163.com>
2025-01-11 23:09:51 +0800

    Upload changes

--- app/api/v1/controller/BinanceController.php
+++ app/api/v1/controller/BinanceController.php
@@ -15,15 +15,19 @@ class BinanceController extends Controller
     //币安控制器
     public function account()
     {
-        $data = json_decode(file_get_contents('php://input'), true);
-        $user = self::validateJWT();
-        $key = '466SioaRMJGXfKESRF8mdGzICXDN4bv21TP2KzFYqx6AFMq3TNCiYNYY9TV6Aq32y';
-        $secret = 'FKEYTbtFUWKyoIrdeaGEsM1LYximNToGWGhyG5hWgWQOSEqGN4wxCE7h5eCzGqOE';
-        $client = new Spot(['key' => $key, 'secret' => $secret]);;
-        $response = $client->account();
-        //uid   是否允许交易   账户类型
-        echo ($response);
+        try {
+            $data = json_decode(file_get_contents('php://input'), true);
+            $user = self::validateJWT();
+            $key = '466SioaRMJGXfKESRF8mdGzICXDN4bv21TP2KzFYqx6AFMq3TNCiYNYY9TV6Aq32y';
+            $secret = 'FKEYTbtFUWKyoIrdeaGEsM1LYximNToGWGhyG5hWgWQOSEqGN4wxCE7h5eCzGqOE';
+            $client = new Spot(['key' => $key, 'secret' => $secret]);;
+            $response = $client->account();
+            //uid   是否允许交易   账户类型
+            echo ($response);
 
-        echo dump($response->uid, $response->canTrade, $response->accountType);
+            echo dump($response->uid, $response->canTrade, $response->accountType);
+        } catch (\Throwable $th) {
+            dump($th);
+        }
     }
 }
