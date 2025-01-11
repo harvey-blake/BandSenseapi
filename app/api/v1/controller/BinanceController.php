70f0f9af2c11e7250e7049705aa199e754a08bed
dexcedu <shicms@163.com>
2025-01-11 23:38:26 +0800

    Upload changes

--- app/api/v1/controller/BinanceController.php
+++ app/api/v1/controller/BinanceController.php
@@ -18,24 +18,18 @@ class BinanceController extends Controller
         try {
             $data = json_decode(file_get_contents('php://input'), true);
             $user = self::validateJWT();
-            $key = '466SioaRMJGXfKESRF8mdGzICXDN4bv21TP2KzFYqx6AFMq3TNCiYNYY9TV6Aq32y';
+            $key = '466SioaRMJGXfKESRF8mdGzICXDN4bv21TP2KzFYqx6AFMq3TNCiYNYY9TV6Aq32';
             $secret = 'FKEYTbtFUWKyoIrdeaGEsM1LYximNToGWGhyG5hWgWQOSEqGN4wxCE7h5eCzGqOE';
             $client = new Spot(['key' => $key, 'secret' => $secret]);;
             $response = $client->account();
             //uid   是否允许交易   账户类型
             echo ($response);
 
-            // echo dump($response->uid, $response->canTrade, $response->accountType);
+            echo dump($response->uid, $response->canTrade, $response->accountType);
         } catch (\Binance\Exception\ClientException $th) {
-            // 打印异常的类型和消息
-            dump(get_class($th)); // 打印异常的完整类名
-            dump($th->getMessage()); // 打印异常消息
 
-            // 获取响应体并解析错误的 JSON 数据
-            $errorDetails = json_decode($th, true);;
-
-            // 打印解析后的错误数据
-            dump($errorDetails);  // 输出 {"code": -2014, "msg": "API-key format invalid."}
+            // dump($th->getMessage()); // 打印异常消息
+            echo json_encode(retur('失败', '$key或$secret错误', -2014));
         }
     }
 }
