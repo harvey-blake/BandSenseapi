a777f02d8356fc60b0e0b9146906e0d7a9a33fa8
Harvey Blake <shicms@163.com>
2025-01-14 13:01:10 +0800

    Upload changes

--- app/api/v1/controller/QueryController.php
+++ app/api/v1/controller/QueryController.php
@@ -23,15 +23,21 @@ class QueryController extends Controller
             echo json_encode(retur($count, $arr, 422));
         }
     }
-    public function Timestamp()
+    public function Profit()
     {
-        $currentDateTime = date('Y-m-d H:i:s');
-        dump($currentDateTime);
-        $count =  Db::table('Strategy')->where(['time <' => $currentDateTime])->select();
-        dump($count);
-        $count =  Db::table('Strategy')->where(['time >' => $currentDateTime])->select();
-        dump($count);
-        $count =  Db::table('Strategy')->where(['time <' => $currentDateTime])->SUM('unitprice');
-        dump($count);
+        try {
+            $user = self::validateJWT();
+            $todayStart = date('Y-m-d 00:00:00');  // 今日开始时间
+            $todayEnd = date('Y-m-d 23:59:59');    // 今日结束时间
+            $monthStart = date('Y-m-01 00:00:00');  // 本月开始时间
+            $monthEnd = date('Y-m-t 23:59:59');     // 本月结束时间
+            $todasum =  Db::table('Strategy')->where(['userid' => $user['id'], 'time >=' => $todayStart, 'time <=' => $todayEnd])->SUM('unitprice') ?? 0;
+            $monthsum =  Db::table('Strategy')->where(['userid' => $user['id'], 'time >=' => $monthStart, 'time <=' => $monthEnd])->SUM('unitprice') ?? 0;
+            $pastProfit =  Db::table('Strategy')->where(['userid' => $user['id']])->SUM('unitprice') ?? 0;
+
+            echo json_encode(retur('成功', ['todasum' => $todasum, 'monthsum' => $monthsum, 'pastProfit' => $pastProfit]));
+        } catch (\Throwable $th) {
+            echo json_encode(retur('失败', '未知错误', 409));
+        }
     }
 }
