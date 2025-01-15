ca14505e22837bcf360b11b9b3c60f3f35dcafdf
Harvey Blake <shicms@163.com>
2025-01-15 11:24:07 +0800

    Upload changes

--- app/api/v1/controller/DeleteController.php
+++ app/api/v1/controller/DeleteController.php
@@ -32,4 +32,18 @@ class  DeleteController extends Controller
             echo json_encode(retur('失败', '没删除任何数据', 404));
         }
     }
+    public function binancekey()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        $user = self::validateJWT();
+
+
+        $arr =  Db::table('Strategy')->where(['keyid' => $data['keyid'], 'userid' => $user['id']])->delete();
+        $arr =  Db::table('binance_key')->where(['id' => $data['keyid'], 'userid' => $user['id']])->delete();
+        if ($arr > 0) {
+            echo json_encode(retur('成功', $arr));
+        } else {
+            echo json_encode(retur('失败', '没删除任何数据', 404));
+        }
+    }
 }
