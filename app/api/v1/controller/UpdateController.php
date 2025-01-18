9611f8b6e21c03a0e6f780c09d70a5aef923ad9e
Harvey Blake <shicms@163.com>
2025-01-18 19:06:04 +0800

    Upload changes

--- app/api/v1/controller/UpdateController.php
+++ app/api/v1/controller/UpdateController.php
@@ -9,6 +9,9 @@ use function common\retur;
 use function common\sendMessage;
 use function common\Message;
 use function common\tgverification;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 use common\Controller;
 
 class  UpdateController extends Controller
@@ -29,6 +32,50 @@ class  UpdateController extends Controller
         } catch (\Throwable $th) {
 
             echo json_encode(retur('失败', '非法访问', 500));
+        }
+    }
+
+    public function subscription()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        $template_path = __DIR__ . '/../mail/subscription.html'; // 替换为模板文件的实际路径
+
+        $template_content = file_get_contents($template_path);
+
+        self::mail('3005779@qq.com', '恭喜您订阅成功', $template_content);
+        // echo json_encode(retur('成功', self::mail($data['mail'], '恭喜您,订阅成功', $template_content)));
+    }
+
+
+
+    public function mail($to, $title, $content)
+    {
+        $mail = new PHPMailer();
+        try {
+            $mail = new PHPMailer();
+            // 配置邮件服务器设置
+            // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+            $state =  Db::table('mailsmpt')->field('*')->where(['id' => 1])->find();
+            $mail->isSMTP(); // 使用 SMTP 发送
+            $mail->Host = $state['Host']; // 设置 SMTP 服务器地址
+            $mail->SMTPAuth = true; // 启用 SMTP 认证
+            $mail->Username = $state['Username']; // SMTP 用户名
+            $mail->Password = $state['Password']; // SMTP 密码
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port = 465; // 设置 SMTP 端口号
+            // 设置字符编码为 UTF-8
+            $mail->CharSet = 'UTF-8';
+            // 设置邮件内容
+            $mail->setFrom('dexcpro@gmail.com', 'DEXC'); // 发件人邮箱和姓名
+            $mail->addAddress($to); // 收件人邮箱和姓名
+            $mail->isHTML(true);
+            $mail->Subject = $title; // 邮件主题
+            $mail->Body =  $content; // 邮件正文
+            return $mail->send();
+        } catch (Exception $e) {
+            return false;
+            // return  $mail->ErrorInfo;
         }
     }
 }
