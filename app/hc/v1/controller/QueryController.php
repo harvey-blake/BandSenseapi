6a667ac88c2f5d4bb29c70e5b4831e9246f802d4
Harvey Blake <shicms@163.com>
2025-02-22 04:34:34 +0800

    Upload changes

--- app/hc/v1/controller/QueryController.php
+++ app/hc/v1/controller/QueryController.php
@@ -130,7 +130,7 @@ class QueryController extends Controller
         // 获取交易详情
         $myCallback = new CallbackController();
         $web3->eth->getTransactionByHash($txHash, $myCallback);
-        dump($myCallback->result);
+        dump($myCallback->result->value->toString());
 
 
         $enabi = new Ethabi([
