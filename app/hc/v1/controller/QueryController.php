6b47f590b0b5a5b6fbc802378c452cd2e4dd0c0b
Harvey Blake <shicms@163.com>
2025-02-22 04:20:20 +0800

    Upload changes

--- app/hc/v1/controller/QueryController.php
+++ app/hc/v1/controller/QueryController.php
@@ -144,11 +144,8 @@ class QueryController extends Controller
         ]);
         $types = ['uint256'];
         $decoded = $enabi->decodeParameters($types, $myCallback->result->value);
-
-        dump($decoded[0]->toString());
-
-
-        // 获取 Wei 转换后的值，假设 $weiValue 是已经拼接好的 Wei 值
-        $convertedValue = Utils::fromWei($decoded[0], 'ether');
+        dump($decoded);
+        $value =  Utils::fromWei($decoded[0], 'ether')->toString();
+        dump($value);
     }
 }
