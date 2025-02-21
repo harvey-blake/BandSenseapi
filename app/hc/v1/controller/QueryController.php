caec967c46e690dd5d0b8dec42da76e7a1195ef1
Harvey Blake <shicms@163.com>
2025-02-22 03:34:26 +0800

    Upload changes

--- app/hc/v1/controller/QueryController.php
+++ app/hc/v1/controller/QueryController.php
@@ -145,7 +145,7 @@ class QueryController extends Controller
         $types = ['uint256'];
         $decoded = $enabi->decodeParameters($types, $myCallback->result->value);
         dump($decoded);
-        $value =  Utils::fromWei($decoded[0], 'ether')[0]->toString();
+        $value =  Utils::fromWei($decoded[0], 'ether')->toString();
         dump($value);
     }
 }
