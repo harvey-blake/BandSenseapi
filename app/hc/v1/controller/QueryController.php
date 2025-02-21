9630aa6febe578dbcfe95b0b9d390b9166a28d3b
Harvey Blake <shicms@163.com>
2025-02-22 05:12:17 +0800

    Upload changes

--- app/hc/v1/controller/QueryController.php
+++ app/hc/v1/controller/QueryController.php
@@ -153,7 +153,7 @@ class QueryController extends Controller
         $decoded = $enabi->decodeParameters($types, $filtered[0]->data);
         /** @var \phpseclib3\Math\BigInteger[] $decoded */
         dump($decoded[0]->value / 10 ** 18);
-        $ether = bcdiv($decoded[0]->value, 10 ** 18);
+        $ether = bcdiv($decoded[0]->value, 10 ** 18, 18);
         dump($ether);
     }
 }
