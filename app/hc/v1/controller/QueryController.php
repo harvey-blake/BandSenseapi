1d4fbdb1113c836e36d39aded7adc4c09e574a14
Harvey Blake <shicms@163.com>
2025-02-22 05:17:24 +0800

    Upload changes

--- app/hc/v1/controller/QueryController.php
+++ app/hc/v1/controller/QueryController.php
@@ -137,6 +137,7 @@ class QueryController extends Controller
             dump($item);
             return $item->address == '0xc2132d05d31c914a87c6611c10748aeb04b58e8f';
         });
+        $filtered = array_values($filtered);
         dump($filtered);
 
         $enabi = new Ethabi([
