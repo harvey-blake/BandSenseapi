bc8c77b77dbe66ea01817d7c8b5c80504dddcc95
Harvey Blake <shicms@163.com>
2025-02-22 09:17:19 +0800

    Upload changes

--- app/hc/v1/controller/QueryController.php
+++ app/hc/v1/controller/QueryController.php
@@ -187,8 +187,9 @@ class QueryController extends Controller
             $field1 = substr($data, 0, 64);
             $decoded = $enabi->decodeParameters($types, $field1);
             /** @var \phpseclib3\Math\BigInteger[] $decoded */
-            $ether = bcdiv($decoded[0]->value, 10 ** 18);
-            dump($ether);
+            $result = bcdiv($decoded[0]->value, 10 ** 18, 18);
+            $result = rtrim(rtrim($result, '0'), '.');
+            dump($result);  //马蹄数量
         }
     }
 }
