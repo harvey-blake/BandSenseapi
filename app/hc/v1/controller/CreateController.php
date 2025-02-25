afa8143222dd1b1c614ea806bfae73171634bf07
Harvey Blake <shicms@163.com>
2025-02-25 09:42:09 +0800

    Upload changes

---
+++ app/hc/v1/controller/CreateController.php
@@ -0,0 +1,46 @@
+<?php
+// 所有自定义控制器的基本控制器,应该继承自它
+// 添加（插入）：
+
+// 数据格式不正确或验证失败：422
+// 客户端请求存在问题：400
+// 修改（更新）：
+
+// 资源状态的冲突：409
+// 数据格式不正确或验证失败：422
+// 客户端请求存在问题：400
+// 删除：
+
+// 资源未找到：404
+// 客户端请求存在问题：400
+// 查询：
+
+// 资源未找到：404
+// 客户端请求存在问题：400
+namespace app\hc\v1\controller;
+
+use Db\Db;
+use function common\dump;
+use function common\retur;
+use function common\tgverification;
+use common\Controller;
+// 写入
+class CreateController extends Controller
+{
+    public function onaddress()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        $hash = tgverification($data['hash']);
+        $arr =  Db::table('tokenlist')->field('*')->where(['chain' => $data['chain'], 'address' => $data['token']])->find();
+        if (!$arr) {
+            $arr =  Db::table('onaddress')->insert(['chain' => $data['chain'], 'address' => $data['address'], 'userid' => $hash['id'], 'tokenname' => $arr['name'], 'token' => $data['token']]);
+            if ($arr > 0) {
+                echo json_encode(retur('成功', $arr));
+            } else {
+                echo json_encode(retur('失败', '添加失败请查看参数', 422));
+            }
+        } else {
+            echo json_encode(retur('失败', '交易对已经存在', 409));
+        }
+    }
+}
