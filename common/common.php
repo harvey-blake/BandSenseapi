dc2399508cd4f97042a8b8d76640b843aa938c6c
Harvey Blake <shicms@163.com>
2025-02-25 10:38:25 +0800

    Upload changes

--- common/common.php
+++ common/common.php
@@ -54,41 +54,47 @@ function tgverification($data)
 {
 
     // 解析接收到的 URL 编码的数据
+    try {
+        $botToken = '7949382682:AAGhPeyqz4ru183scmko8bIjdxp37G3Bs0k'; // 替换为你的 Bot Token
 
-    $botToken = '7949382682:AAGhPeyqz4ru183scmko8bIjdxp37G3Bs0k'; // 替换为你的 Bot Token
+        // 解码接收到的URL编码数据
+        $decodedString = urldecode($data);
 
-    // 解码接收到的URL编码数据
-    $decodedString = urldecode($data);
+        // 将解码后的数据转换为数组
+        parse_str($decodedString, $params);
 
-    // 将解码后的数据转换为数组
-    parse_str($decodedString, $params);
+        // 提取并移除 'hash' 参数
+        $receivedHash = $params['hash'];
+        unset($params['hash']);
 
-    // 提取并移除 'hash' 参数
-    $receivedHash = $params['hash'];
-    unset($params['hash']);
+        // 按字母顺序对剩余的参数进行排序
+        ksort($params);
 
-    // 按字母顺序对剩余的参数进行排序
-    ksort($params);
+        // 生成数据检查字符串，使用换行符分隔
+        $dataCheckString = '';
+        foreach ($params as $key => $value) {
+            $dataCheckString .= "$key=$value\n";
+        }
+        $dataCheckString = rtrim($dataCheckString); // 移除最后一个换行符
 
-    // 生成数据检查字符串，使用换行符分隔
-    $dataCheckString = '';
-    foreach ($params as $key => $value) {
-        $dataCheckString .= "$key=$value\n";
-    }
-    $dataCheckString = rtrim($dataCheckString); // 移除最后一个换行符
+        // 生成 secretKey：将 botToken 作为密钥生成 HMAC 的 secretKey
+        $secretKey = hash_hmac('sha256', $botToken, 'WebAppData', true);
 
-    // 生成 secretKey：将 botToken 作为密钥生成 HMAC 的 secretKey
-    $secretKey = hash_hmac('sha256', $botToken, 'WebAppData', true);
+        // 生成 HMAC-SHA256 的哈希值
+        $calculatedHash = hash_hmac('sha256', $dataCheckString, $secretKey);
 
-    // 生成 HMAC-SHA256 的哈希值
-    $calculatedHash = hash_hmac('sha256', $dataCheckString, $secretKey);
-
-    // 比较哈希值，判断数据是否有效
-    if (hash_equals($calculatedHash, $receivedHash)) {
-        $params = json_decode($params['user'], true);
-        return $params;
-    } else {
-        return false;
+        // 比较哈希值，判断数据是否有效
+        if (hash_equals($calculatedHash, $receivedHash)) {
+            $params = json_decode($params['user'], true);
+            return $params;
+        } else {
+            echo json_encode(retur('错误', '非法访问', 403));
+            exit;
+        }
+    } catch (\Throwable $th) {
+        // 账号未登录
+        echo json_encode(retur('错误', '非法访问', 403));
+        exit;
     }
 }
 function sendMessage($chat_id, $message)
