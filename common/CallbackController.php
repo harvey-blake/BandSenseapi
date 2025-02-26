3a39ed4126a40c0f8ba131e7c28c18315c3e8d48
Harvey Blake <shicms@163.com>
2025-02-27 06:59:19 +0800

    Upload changes

--- common/CallbackController.php
+++ common/CallbackController.php
@@ -11,6 +11,17 @@ class CallbackController
         if ($error) {
             throw $error;
         }
-        $this->result = $result;
+
+        // 处理可能是 BigInteger 或 GMP 类型的数据
+        if ($result instanceof \phpseclib\Math\BigInteger) {
+            // 提取 BigInteger 的值并将其转化为字符串
+            $this->result = $result->toString();
+        } elseif ($result instanceof GMP) {
+            // 如果是 GMP 对象，使用 gmp_strval 转换为字符串
+            $this->result = gmp_strval($result);
+        } else {
+            // 其他类型的处理
+            $this->result = $result;
+        }
     }
 }
