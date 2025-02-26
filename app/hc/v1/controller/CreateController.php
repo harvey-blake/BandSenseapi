8a51c492afa29afe2d8e83d1abbfc3829233783e
Harvey Blake <shicms@163.com>
2025-02-27 06:32:53 +0800

    Upload changes

--- app/hc/v1/controller/CreateController.php
+++ app/hc/v1/controller/CreateController.php
@@ -124,6 +124,8 @@ class CreateController extends Controller
             $abi = Db::table('abi')->field('*')->where(['name' => 'erc20'])->find();
             $contract = new Contract($web3->provider, $abi);
             // 查询余额
+            dump($contract);
+
             $contract->at('0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063')->call('balanceOf', '0x6a7f9a2592f4a942c44712f829e5018e6d668a3d', $myCallback);
             // 处理结果(可能每个代币都不一样，到时候需要修改的)
             $balance =  $myCallback->result['balance']->value;
