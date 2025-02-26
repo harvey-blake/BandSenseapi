42385b9d548a8603218c6bbc0dfb3778ca6dfbc4
Harvey Blake <shicms@163.com>
2025-02-27 07:58:27 +0800

    Upload changes

--- app/hc/v1/controller/CreateController.php
+++ app/hc/v1/controller/CreateController.php
@@ -145,7 +145,7 @@ class CreateController extends Controller
                     'type' => 'function'
                 ]
             ];
-            $contract = new Contract($web3->provider, $abi);
+            $contract = new Contract('https://polygon-mainnet.g.alchemy.com/v2/s6pD2K_z7MFUSCP2xLkUEizCYM4_z3Hb', $abi);
 
             dump('?');
             $contract->at('0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063')->call('totalSupply',  function ($err, $version) {
