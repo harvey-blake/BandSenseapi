91e351703e0be14f792fb7687f863df2d723008e
Harvey Blake <shicms@163.com>
2025-03-01 18:49:19 +0800

    Upload changes

--- app/hc/v1/controller/TokenController.php
+++ app/hc/v1/controller/TokenController.php
@@ -47,9 +47,27 @@ class TokenController extends Controller
         $chatId = "1882040053";               // 替换成你的 Chat ID
 
         // 要发送的消息
-        $message = "[inline URL](http://www.example.com/)";
+        $message = "*bold \\*text*\n"
+            . "_italic \\*text_\n"
+            . "__underline__\n"
+            . "~strikethrough~\n"
+            . "||spoiler||\n"
+            . "*bold _italic bold ~italic bold strikethrough ||italic bold strikethrough spoiler||~ __underline italic bold___ bold*\n"
+            . "[inline URL](http://www.example.com/)\n"
+            . "[inline mention of a user](tg://user?id=123456789)\n"
+            . "`inline fixed-width code`\n"
+            . "```\npre-formatted fixed-width code block\n```\n"
+            . "```python\npre-formatted fixed-width code block written in the Python programming language\n```\n"
+            . ">Block quotation started\n"
+            . ">Block quotation continued\n"
+            . "**>The expandable block quotation started right after the previous block quotation\n"
+            . ">It is separated from the previous block quotation by an empty bold entity\n"
+            . ">Expandable block quotation continued\n"
+            . ">Hidden by default part of the expandable block quotation started\n"
+            . ">Expandable block quotation continued\n"
+            . ">The last line of the expandable block quotation with the expandability mark||\n";
 
-        // 转义 MarkdownV2 特殊字符
+        // 转义 MarkdownV2 特殊字符（保留格式）
         $specialChars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
         foreach ($specialChars as $char) {
             $message = str_replace($char, '\\' . $char, $message);
