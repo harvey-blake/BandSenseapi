4131aef5651be31910a5cd32d2eda0598ad82db7
Harvey Blake <shicms@163.com>
2025-03-01 19:31:06 +0800

    Upload changes

--- app/hc/v1/controller/TokenController.php
+++ app/hc/v1/controller/TokenController.php
@@ -62,7 +62,18 @@ pre-formatted fixed-width code block
 ```
 ```python
 pre-formatted fixed-width code block written in the Python programming language
-```";
+```
+>Block quotation started
+>Block quotation continued
+>Block quotation continued
+>Block quotation continued
+>The last line of the block quotation
+**>The expandable block quotation started right after the previous block quotation
+>It is separated from the previous block quotation by an empty bold entity
+>Expandable block quotation continued
+>Hidden by default part of the expandable block quotation started
+>Expandable block quotation continued
+>The last line of the expandable block quotation with the expandability mark||";
 
         // 只转义会破坏 MarkdownV2 格式的特殊字符
 
