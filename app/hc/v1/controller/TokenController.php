5c2209b1c200376dd733692a619dc1230f6c0b52
Harvey Blake <shicms@163.com>
2025-03-01 18:36:18 +0800

    Upload changes

--- app/hc/v1/controller/TokenController.php
+++ app/hc/v1/controller/TokenController.php
@@ -47,16 +47,7 @@ class TokenController extends Controller
         $chatId = "1882040053";               // 替换成你的 Chat ID
 
         // 要发送的消息
-        $message = "*bold \*text*
-_italic \*text_
-__underline__
-~strikethrough~
-||spoiler||
-*bold _italic bold ~italic bold strikethrough ||italic bold strikethrough spoiler||~ __underline italic bold___ bold*
-[inline URL](http://www.example.com/)
-[inline mention of a user](tg://user?id=123456789)
-![👍](tg://emoji?id=5368324170671202286)
-`inline fixed-width code`";
+        $message = `inline fixed-width code`;
 
         // 转义 MarkdownV2 特殊字符
         $specialChars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
