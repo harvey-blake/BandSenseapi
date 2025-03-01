0c03449190d3f209ddabe1ca2ba8bd1cb8ba5b0b
Harvey Blake <shicms@163.com>
2025-03-01 18:38:28 +0800

    Upload changes

--- app/hc/v1/controller/TokenController.php
+++ app/hc/v1/controller/TokenController.php
@@ -47,7 +47,7 @@ class TokenController extends Controller
         $chatId = "1882040053";               // 替换成你的 Chat ID
 
         // 要发送的消息
-        $message = "`inline fixed-width code`";
+        $message = "[inline URL](http://www.example.com/)";
 
         // 转义 MarkdownV2 特殊字符
         $specialChars = ['_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!'];
