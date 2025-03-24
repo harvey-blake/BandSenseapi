c6f6c7e3464ad96b0d4d584e61a846d1617271d5
Harvey Blake <shicms@163.com>
2025-03-24 08:06:11 +0800

    Upload changes

--- app/api/v2/controller/CreateController.php
+++ app/api/v2/controller/CreateController.php
@@ -66,7 +66,7 @@ class CreateController extends Controller
 
         $jieguo =  Db::table('mailcode')->field('*')->where(['mail' =>  $mail, 'time >=' => $time])->count();
         dump($jieguo);
-        $arr =  Db::table('mailcode')->where(['mail' => $mail])->order('id', 'DESC')->limit(1)->find();
+        $arr =  Db::table('mailcode')->where(['mail' => $mail])->order('time', 'DESC')->limit('1')->find();
         dump($arr);
     }
 }
