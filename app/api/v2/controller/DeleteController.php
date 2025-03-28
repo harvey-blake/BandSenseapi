0ddb04eecd11c4bab7c521b67f6ed2dfa477dc6f
Harvey Blake <shicms@163.com>
2025-03-28 09:08:15 +0800

    Upload changes

--- app/api/v2/controller/DeleteController.php
+++ app/api/v2/controller/DeleteController.php
@@ -8,4 +8,18 @@ use function common\dump;
 use function common\retur;
 use common\Controller;
 
-class  DeleteController extends Controller {}
+class  DeleteController extends Controller
+{
+
+    public function Strategy()
+    {
+        $data = json_decode(file_get_contents('php://input'), true);
+        $user = self::isvalidateJWT();
+        $arr =  Db::table('Strategy')->where(['id' => $data['id'], 'userid' => $user['id']])->delete();
+        if ($arr > 0) {
+            echo json_encode(retur('成功', $arr));
+        } else {
+            echo json_encode(retur('失败', '没删除任何数据', 404));
+        }
+    }
+}
